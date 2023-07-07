53186bddb797f6de49f74efa61373cc793687729
Anshul Singh <73996817+anshulsc@users.noreply.github.com>
2023-07-07 16:06:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ import scipy.fft as sp
 import plotly.colors
 import matplotlib.pyplot as plt
 from io import BytesIO
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, LearningCurveDisplay, ShuffleSplit
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import LearningCurveDisplay, ShuffleSplit
 
 
 
