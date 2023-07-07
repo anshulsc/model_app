d6671ec3c6053b87d5949fa15a610596cb61d9d8
Anshul Singh <73996817+anshulsc@users.noreply.github.com>
2023-07-07 16:02:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import LearningCurveDisplay, ShuffleSplit
 
 
 
